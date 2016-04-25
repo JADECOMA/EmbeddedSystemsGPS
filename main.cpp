35cd848b6299ada1846c8c5d3ec5a04b3899e500
Manuel Härtle <info@jadecoma.de>
2016-04-25 13:33:59 +0200

    Alle Testfälle können nun in Stapelverarbeitung getestet werden

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include "Decoder/Decoder.h"
+#include <iostream>
+#include <cstring>
+
+using namespace std;
 
 int main(int argc, char **argv) {
     Decoder *decoder = new Decoder();
-    decoder->decode(argv[1]);
+
+    int i;
+    string file;
+    char fileName[200];
+
+    for (i = 1; i < 31; i++) {
+        //decoder->decode(argv[1]);
+
+        file = string("C:\\Users\\Manuel\\ClionProjects\\EmbeddedSystemsGPS\\gps_sequence_") + string(to_string(i)) + string(".txt");
+        strncpy(fileName, file.c_str(), sizeof(fileName));
+        cout << fileName << endl;
+        decoder->decode(fileName);
+        cout << endl;
+    }
+
 
     return 0;
 }
